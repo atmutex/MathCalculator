d2f7273b8c48fbbd97aab257d9cf405dfd469198
atmutex <mutexsharma@gmail.com>
2021-02-22 01:34:41 +0530

    Minor Fixes

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -7,20 +7,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+import android.util.Log;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class MathCalculator extends CordovaPlugin {
 
+private static final String TAG = "<------- ZoomIonicAngularPlugin ---------->";
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException 
     {
         if(action.equals("add"))
         {
             this.add(args,callbackContext);
+            Log.v(TAG, "add................................");
+			Log.d(TAG, "add................................");
             return true;
         }else if(action.equals("subtract"))
         {
+            Log.v(TAG, "subtract................................");
+			Log.d(TAG, "subtract................................");
             this.subtract(args,callbackContext);
             return true;
         }
@@ -36,6 +44,9 @@ public class MathCalculator extends CordovaPlugin {
             {
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+
+                            Log.v(TAG, "addf................................");
+			Log.d(TAG, "addf................................");
 
                 callback.success("" + (p1+p2) );
 
